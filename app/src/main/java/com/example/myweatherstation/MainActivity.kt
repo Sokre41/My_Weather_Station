58839afadc9170200307985380273f58d4bc2f26
Krešimir <sokre41@gmail.com>
2022-04-20 15:34:26 +0200

    Add MainActivity UI

--- app/src/main/java/com/example/myweatherstation/MainActivity.kt
+++ app/src/main/java/com/example/myweatherstation/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myweatherstation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myweatherstation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.submitLocation.setOnClickListener {
+            Toast.makeText(this, "Ok", Toast.LENGTH_SHORT).show()
+        }
     }
 }
